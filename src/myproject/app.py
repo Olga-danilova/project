8ca41df46bb52d0d7b9fd852e90fa050dbe869ec
Olga-danilova <124255405+Olga-danilova@users.noreply.github.com>
2025-12-17 18:15:41 +0300

    Initial commit

---
+++ src/myproject/app.py
@@ -0,0 +1,8 @@
+from src.myproject.utils import greet, fetch_status
+
+def main():
+    print(greet("World"))
+    print(fetch_status("https://example.com"))
+
+if __name__ == "__main__":
+    main()
